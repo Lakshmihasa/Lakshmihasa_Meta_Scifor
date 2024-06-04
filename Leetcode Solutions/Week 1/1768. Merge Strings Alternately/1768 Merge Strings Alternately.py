28889b06a4d2f69136a102142fc3dd5cd86dafeb
Lakshmi Hasa <lakshmihasa2003@gmail.com>
2024-06-04 15:48:42 +0530

    Delete Leetcode Solutions/Week 1/1768. Merge Strings Alternately directory

--- Leetcode Solutions/Week 1/1768. Merge Strings Alternately/1768 Merge Strings Alternately.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        merged = ''
-        i = 0
-        while i < len(word1) and i < len(word2):
-            merged += word1[i] + word2[i]
-            i += 1
-        merged += word1[i:] + word2[i:]
-        return merged
