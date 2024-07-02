6183b9642cb7a2f61489a691736cc43cf9a712cb
Lakshmi Hasa <lakshmihasa2003@gmail.com>
2024-07-02 16:20:50 +0530

    Delete Leetcode solutions/Week 1 directory

--- Leetcode solutions/Week 1/28. Find the Index of the First Occurrence in a String/28. Find the Index of the First Occurrence in a String.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def strStr(self, haystack, needle):
-        if needle not in haystack:
-            return -1
-        else:
-            n=len(needle)
-            for i in range(len(haystack)):
-                if haystack[i:i+n]==needle:
-                    return i
